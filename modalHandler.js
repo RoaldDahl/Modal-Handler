0e7202f9b95de7cebc6c94da69c67965aea880c4
RoaldDahl <95515434+RoaldDahl@users.noreply.github.com>
2022-08-30 19:32:41 +0930

    Update modalHandler.js

--- modalHandler.js
+++ modalHandler.js
@@ -10,8 +10,8 @@ async function loadModals(client) {
     if (!modal.id) return;
 
     client.modals.set(modal.id, modal);
-    table.setHeading(`Folder`, `Modal ID`, `Status`);
-    table.addRow(`${modal.folder}`, `${modal.id}`, "🟩 Success");
+    table.setHeading(`Modal ID`, `Status`);
+    table.addRow(`${modal.id}`, "🟩 Success");
   });
   
   return console.log(table.toString())
